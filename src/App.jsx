812a4e4d0b6b8436b94ff74aa8f1a0efd0b98d30
Artur <arturbrnd1@gmail.com>
2025-11-23 13:51:56 -0300

    sidebar component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import { Header } from './components/Header.jsx';
+import { Post } from './Post.jsx';
+import styles from './App.module.css';
+
 import './global.css';
+import { Sidebar } from './components/Sidebar.jsx';
 
 export function App() {
   return (
     <div>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author='Diego Fernandes'
+            content='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem culpa quas ad distinctio ipsam ipsa adipisci inventore laborum, architecto assumenda quia numquam mollitia possimus soluta voluptatum amet accusantium vero minima.'
+          />
+          <Post author='Gabriel Buzzi' content='Um novo post muito legal' />
+        </main>
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,33 @@
+import { PencilLine } from 'phosphor-react';
+
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src='https://images.unsplash.com/photo-1605379399642-870262d3d051?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        alt=''
+      />
+
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src='https://i.pinimg.com/736x/b5/89/7d/b5897d1aa164ea5053165d4a04c2f2fa.jpg'
+          alt=''
+        />
+
+        <strong>Diego Fernandes</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href='#'>
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
